146708c2453fffee23cf4d8f22bdff9bc139d38c
unknown <tdhtp2016@gmail.com>
2022-03-30 18:14:17 +0700

    ok

--- 18-forkify/final/src/js/config.js
+++ 18-forkify/final/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR KEY>';
+export const KEY = '787d1b77-091b-4527-bfd7-d7d84c0f1d86';
 export const MODAL_CLOSE_SEC = 2.5;
